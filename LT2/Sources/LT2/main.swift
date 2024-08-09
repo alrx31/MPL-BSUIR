0769e79956ed468ecca1523bc852b75c3e6ebcae
Usov <usov.lesha2017@gmail.com>
2024-08-09 15:57:12 +0300

    add changes

--- LT2/Sources/LT2/main.swift
+++ LT2/Sources/LT2/main.swift
@@ -1,9 +1,57 @@
 import task1Lib
 import task2Lib
-import Swift
-print(factorial(p:1))
-print(factorial(p:5))
-print(isPalendromeString("asa"))    
-print(isPalendromeString("asas"))    
-print(isPalendromeInt(12))    
-print(isPalendromeInt(121))    
+
+
+while (true){
+    print("Выберите номер задания, 3 - выход")
+    var value = 0
+    if let input = readLine(){
+        if let number = Int(input){
+            if number <= 2 || number >= 1{
+                value = number
+            }else{
+                print("неверный номер задания")
+            }
+        }else{
+            print("неверный ввод")
+        }
+    }else{
+        print("неверный ввод")
+    }
+
+    if value != 0{
+        if value == 3{
+            break
+        }
+        if(value == 1){
+            print("Введите число для нахождения факрориала")
+            if let input1 = readLine() {
+                if let number1 = Int(input1) {
+                    let res = factorial(p: number1)                
+                    print(res)
+                }else{
+                    print("нужно ввести число")
+                }
+            }else{
+                print("неверный ввод")
+            }
+        }
+        if(value == 2){
+            print("Введите строку для определения полиндрома")
+            if let input1 = readLine() {
+                let res = isPalendromeString(input1)
+                if res {
+                    print(input1 + " является полиндромом")
+                }else{
+                    print(input1 + " не является полиндромом")
+                }
+            }else{
+                print("неверный ввод")
+            }
+        }
+        continue
+
+    }else{
+        print("не введен номер задания")
+    }
+}
